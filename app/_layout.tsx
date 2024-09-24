372046d2dbadae1cddd867787bd5f8d4da316bca
Eric Durocher <erciduro@gmail.com>
2024-09-23 21:31:44 -0400

    supp clerk warning

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,15 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
 import { tokenCache } from '@/lib/auth';
+import { LogBox } from 'react-native';
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
     throw new Error('Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env');
 }
+
+LogBox.ignoreLogs(['Clerk:']);
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
